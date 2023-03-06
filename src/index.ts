5aefe8d8aa5a34fa253ab30b70c45fb9ab738d30
Thomas <71586988+Tommypop2@users.noreply.github.com>
2023-03-06 21:49:33 +0000

    Version 0.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-export default function getFontDetails(){
-    
+export function getFontDetails(
+	size: number | string,
+	font: string,
+	strToMeasure?: string
+) {
+	if (strToMeasure == undefined) {
+		strToMeasure = "a";
+	}
+	const canvas = document.createElement("canvas");
+	const ctx = canvas.getContext("2d", { willReadFrequently: true });
+	if (ctx == null) return;
+	ctx.font = `${typeof size === "number" ? size + "px" : size} ${font}`;
+	return ctx.measureText(strToMeasure);
 }
